d9878a94e1e712be6d2fc650c6235cf25003e68b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-10-05 18:01:58 +0700

    Learn component

---
+++ basic-reactjs/src/Component/Class/Beranda.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Beranda extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Ini Adalah Beranda</h1>
+            </div>
+        );
+    }
+}
+export default Beranda;
---
+++ basic-reactjs/src/Component/Functional/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return ( 
+        <div >
+            <h1>Ini Adalah Home wkwkwkwk</h1> 
+        </div>
+    );
+}
+export default Home;
