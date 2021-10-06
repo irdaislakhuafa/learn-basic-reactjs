4482386ddba66f1850b8353d22e1c35499ebc41d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-10-06 17:06:47 +0700

    Learn State in ReactJS

--- basic-reactjs/src/Component/Functional/Home.js
+++ basic-reactjs/src/Component/Functional/Home.js
@@ -1,10 +1,11 @@
+import Produk from "../Class/Produk"
 import Blog from "./Blog"
 
 const Home = () => {
     return ( 
         <div >
-            <h1>Ini Adalah Home wkwkwkwk</h1> 
-            <Blog 
+            {/* <h1>Ini Adalah Home wkwkwkwk</h1>  */}
+            {/* <Blog 
                 tanggal="01 Januari 2002"
                 judul="Teknologi A"
                 description="Awk Awk awk awk awk awk awk awk awk awk awk awk awk sa"
@@ -18,7 +19,11 @@ const Home = () => {
                 tanggal="03 Januari 2002"
                 judul="Teknologi C"
                 description="Awk Awk awk awk awk awk awk awk awk awk awk awk awk sa"
-            />
+            /> */}
+            <h1>Ini Home</h1>
+            <Produk nama="Macbook Pro 2025" harga="30000000" stock="5"/>
+            <Produk nama="Macbook Pro 2025" harga="30000000" stock="5"/>
+            <Produk nama="Macbook Pro 2025" harga="30000000" stock="5"/>
         </div>
     );
 }
