3fe643f5322bb1d2398bf3b703350e58276e54c2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-10-08 22:35:07 +0700

    Create Navbar Component

--- basic-reactjs/src/App.js
+++ basic-reactjs/src/App.js
@@ -4,15 +4,17 @@ import Beranda from './Component/Class/Beranda';
 import BootstrapComp from './Component/Class/BootstrapComp';
 import Parent from './Component/Class/ParentChild/Parent';
 import Home from './Component/Functional/Home';
+import { NavbarComp } from './Component/Functional/RoutingComp/NavbarComp';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Home /> */}
-      {/* <Parent /> */}
-      <BootstrapComp />
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <Home /> */}
+			{/* <Parent /> */}
+			{/* <BootstrapComp /> */}
+			<NavbarComp brand="Irda Islakhu Afa" />
+		</div>
+	);
 }
 
 export default App;
---
+++ basic-reactjs/src/Component/Functional/RoutingComp/NavbarComp.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	NavbarText
+} from 'reactstrap';
+
+export const NavbarComp = (props) => {
+	const [ isOpen, setIsOpen ] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<div>
+			<Navbar color="light" light expand="md">
+				<NavbarBrand href="/" className="mx-4">
+					{props.brand}
+				</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="mr-auto" navbar>
+						<NavItem>
+							<NavLink href="mailto:irdhaislakhuafa@gmail.com">Email</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+						</NavItem>
+						<UncontrolledDropdown nav inNavbar>
+							<DropdownToggle nav caret>
+								Options
+							</DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem>Option 1</DropdownItem>
+								<DropdownItem>Option 2</DropdownItem>
+								<DropdownItem divider />
+								<DropdownItem>Reset</DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+					</Nav>
+					<NavbarText>Simple Text</NavbarText>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
+};
