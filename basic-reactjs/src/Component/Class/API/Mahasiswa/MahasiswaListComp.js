921e7f5939666cf9bd38e92ba65d644dac259428
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-10-28 01:49:20 +0700

    Add realtime delete data

--- basic-reactjs/src/Component/Class/API/Mahasiswa/MahasiswaListComp.js
+++ basic-reactjs/src/Component/Class/API/Mahasiswa/MahasiswaListComp.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, NavLink, Table } from 'reactstrap';
+import { Alert, Button, Container, NavLink, Table } from 'reactstrap';
+import qs from 'querystring';
 
 const apiUrl = 'http://irdaislakhuafa.id/learn-reactjs/simpleapi';
 // const apiUrl = 'http://192.168.12.1:8081/learn-reactjs/simpleapi';
@@ -10,27 +11,68 @@ export default class MahasiswaListComp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			mahasiswa: []
+			mahasiswa: [],
+			message: 'Default message',
+			color: '',
+			display: 'none'
 		};
 	}
 
-	componentDidMount() {
+	getListMahasiswa = () => {
 		console.log('Start GET data mahasiswa from API');
 		axios.get(apiUrl + '/mahasiswa').then((response) => {
+			console.log(response.status);
+			console.log(response);
 			this.setState({
 				mahasiswa: response.data.values
 			});
 			console.log('End GET data mahasiswa from API');
 		});
+	};
+	componentDidMount() {
+		this.getListMahasiswa();
 	}
+
+	// delete method
+	deleteMahasiswa = (idMahasiswa) => {
+		// debuging
+		console.log({ id: idMahasiswa });
+
+		// send request to api
+		axios
+			.delete(apiUrl + '/mahasiswa', {
+				data: {
+					id: idMahasiswa
+				}
+			})
+			.then((response) => {
+				// response delete data
+				console.log(response.status);
+				console.log(response);
+				// this.props.history.push('/mahasiswa'); // failed
+				this.setState({
+					display: 'block',
+					color: 'success',
+					message: 'Berhasil Menghapus Data!'
+				});
+				// after delete data, get all list data back
+				this.getListMahasiswa();
+			});
+	};
+
 	render() {
 		return (
 			<div>
 				<Container>
 					<h1 className="text-center mt-5 pt-4 display-4">List Mahasiswa</h1>
+					<hr className="col-sm-8 offset-sm-2 border border-3 border-success" />
 					<NavLink href="/mahasiswa/tambah">
 						<Button color="success">Tambah Data</Button>
 					</NavLink>
+					{/* alert */}
+					<Alert color={this.state.color} style={{ display: this.state.display }}>
+						{this.state.message}
+					</Alert>
 					<Table className="table table-striped" bordered>
 						{/* header */}
 						<thead>
@@ -69,9 +111,16 @@ export default class MahasiswaListComp extends Component {
 											<Button color="primary">Edit</Button>
 										</Link>
 										{/* hapus */}
-										<Link>
-											<Button color="danger">Hapus</Button>
-										</Link>
+										{/* <Link> */}
+										{/* <a href="/mahasiswa"> */}
+										<Button
+											color="danger"
+											onClick={() => this.deleteMahasiswa(mahasiswa.idMahasiswa)}
+										>
+											Hapus
+										</Button>
+										{/* </a> */}
+										{/* </Link> */}
 									</td>
 								</tr>
 							))}
